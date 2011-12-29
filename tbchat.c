03bc3589c8b4a7ef602a9021d99df8197df87180
Christian Buck <cbuck@lantis.de>
2011-12-29 02:17:02 +0000

    print packets, fixed memset

--- tbchat.c
+++ tbchat.c
@@ -48,6 +48,7 @@ void askQuestions(const struct Question const *q, uint8_t nQuestions,
 void wrapPacket(const unsigned char const *answers, const uint8_t nAnswers, char* packet);
 bool unWrapPacket(unsigned char *answers, const uint8_t nAnswers, char* nick, const char    const * packet);
 uint8_t match(const unsigned char const * a1, const unsigned char const * a2, const    uint8_t n);
+void printPacket(const char const * packet);
 
 void ram(void)
 {
@@ -68,7 +69,7 @@ void ram(void)
         lcdPrintln("Answers read.");
     } else {
         lcdPrintln("Answers not read.");
-        memset(answers, nQuestions, 0); // size in bytes
+        memset(answers, 0, nQuestions); // size in bytes
         initQuestions(q);
         askQuestions(q, nQuestions, answers);
         if(writeFile(ANSWERFILE, (char*)answers, nQuestions) == -1) {
@@ -81,15 +82,16 @@ void ram(void)
     lcdRefresh();
 
     // Encode message (0xD+nick+answers)
-    char packet_send[32], packet_recv[32];
+    char packet_send[32]; //, packet_recv[32];
     wrapPacket(answers, nQuestions, packet_send);
 
     key = BTN_NONE;
     while(key != BTN_ENTER) {
         // Send the message (of love!)
         send_msg((unsigned char*)packet_send);
-
+        char* packet_recv;
         if(recv_msg((unsigned char**)&packet_recv)) {
+            printPacket(packet_recv);
             // decode message
             char recv_nick[17];
             unsigned char recv_answers[nQuestions];
@@ -99,10 +101,13 @@ void ram(void)
                 uint8_t score = match(answers, recv_answers, nQuestions);
 
                 // display!
+                lcdPrintln("Unwrapped:");
                 lcdPrint(recv_nick);
                 lcdPrint(" ");
                 lcdPrint(IntToStr(100*score/nQuestions, 3, 0));
                 lcdPrintln("%");
+            } else {
+                lcdPrintln("cannot read");
             }
         }
         key = getInput();
@@ -141,8 +146,9 @@ void ram(void)
 void wrapPacket(const unsigned char const *answers, const uint8_t nAnswers, char* packet)
 {
     // const uint8_t packetLength = 32;
+    memset(packet, 0, 32);
     uint8_t offset = 0;
-    packet[offset] = 0xD;
+    packet[offset] = 'X';
     offset++;
     const uint8_t nickLength = 16;
     for (uint8_t i=0; i<nickLength; ++i) {
@@ -154,13 +160,18 @@ void wrapPacket(const unsigned char const *answers, const uint8_t nAnswers, char
     }   
 }
 
+void printPacket(const char const * packet) 
+{
+    printWrap(packet, 16);
+    printWrap(packet+18, 16);
+}
 
 bool unWrapPacket(unsigned char *answers, const uint8_t nAnswers, char* nick, const char const * packet)
 {
     // const uint8_t packetLength = 32;
     uint8_t offset = 0;
 
-    if (packet[offset] == 0xD) 
+    if (packet[offset] != 'X')
         return false;
     offset++;
 
@@ -169,7 +180,7 @@ bool unWrapPacket(unsigned char *answers, const uint8_t nAnswers, char* nick, co
     for (uint8_t i=0; i<nickLength; ++i) {
         nick[i] = packet[i+offset];
     }
-    nick[nickLength+1] = 0;
+    nick[nickLength] = 0;
     offset += nickLength;
     
     // read answers
@@ -188,9 +199,9 @@ void askQuestions(const struct Question const *q, uint8_t nQuestions,
         key = getInputWait();
         getInputWaitRelease();
         if(key == BTN_UP) {
-            answers[i] = 1;
+            answers[i] = '1';
         } else if(key == BTN_DOWN) {
-            answers[i] = 0;
+            answers[i] = '0';
         }
     }
 }
@@ -244,7 +255,7 @@ void printWrap(const char const * s, uint8_t len)
 uint8_t recv_msg(unsigned char **msg)
 {
     nrf_config_set(&config);
-    int n = nrf_rcv_pkt_time(10,32,recvbuf);
+    int n = nrf_rcv_pkt_time(100,32,recvbuf);
     if(n != 32)
         return 0;
     *msg = recvbuf;
@@ -254,7 +265,7 @@ uint8_t recv_msg(unsigned char **msg)
 void send_msg(unsigned char *msg)
 {
     memset(sendbuf, 0, 32);
-    for(int i=0; i<32 && msg[i] != 0; ++i) {
+    for(int i=0; i<32; ++i) {
         sendbuf[i] = msg[i];
     }
     nrf_config_set(&config);
